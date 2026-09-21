67db3499eeeadc71f8c09636aeb83ace46dbfd18
Akashh30 <akashnair136@gmail.com>
2026-09-21 13:49:18 +0530

    kuch nahi

--- src/components/Divider2.jsx
+++ src/components/Divider2.jsx
@@ -5,7 +5,7 @@ function Divider2(){
           <div className="w-1/2 mx-auto h-0.5 bg-gradient-to-r from-black/90 via-primary to-black/90 bg-primary shadow-[0_0_85px_#FF1744,0_0_85px_#FF1744]">
 
           </div>
-          </div>
+        </div>
         </>
     )
 }
